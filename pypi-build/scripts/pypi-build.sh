1ca9cdcfece2a7a13afd24c9b5ec1ac55158e8a5
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-08-27 21:10:56 +0100

    Use pypi build docker base

---
+++ pypi-build/scripts/pypi-build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+###############################################################
+#                Unofficial 'Bash strict mode'                #
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/  #
+###############################################################
+set -euo pipefail
+IFS=$'\n\t'
+###############################################################
+
+if [[ -n "${BUILD_DEPENDENCIES:-}" ]]; then
+  apt-get install -y ${BUILD_DEPENDENCIES:-}
+fi
+
+package_name="${1}"
+
+tarball_file_name="$(pypi-download "${package_name}" | awk '{print $2}')"
+
+cwd=$(pwd)
+
+tar xvzf "${cwd}/${tarball_file_name}"
+mv "./$(echo "${cwd}/${tarball_file_name}" | sed 's/.tar.gz//')/"* .
+
+patch_file_path="${cwd}/pypi-build/patches/${package_name}.patch"
+echo "Looking for patch: ${patch_file_path}"
+
+if [[ -f "${patch_file_path}" ]]; then
+  echo "Patch found: ${patch_file_path}"
+  patch_file_args="--stdeb-patch-file=${patch_file_path} "
+fi
+
+python3 setup.py --command-packages stdeb.command sdist_dsc ${patch_file_args:-}$@ --dist-dir=./artifacts bdist_deb
