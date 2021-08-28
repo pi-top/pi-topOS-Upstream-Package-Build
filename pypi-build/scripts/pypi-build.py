290c686fd216134c1d1d46b0fca7ae5dd52e1ccd
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-08-28 18:35:26 +0100

    Fix for packages with intact setup.py

--- pypi-build/scripts/pypi-build.py
+++ pypi-build/scripts/pypi-build.py
@@ -12,38 +12,41 @@ import os
 ###############################
 # Command OR sourced function #
 ###############################
-@click.option(
-    "--options-str",
-    type=str,
-)
 @click.argument(
     "package",
     type=str,
     required=True,
 )
-def main(optionsStr, package):
+@click.option(
+    "--options-str",
+    type=str,
+)
+def main(options_str, package):
     """pi-topOS meta-exec."""
-    click.echo(
-        color.BOLD + color.GREEN + Figlet().renderText("pypi-build") + color.END
-    )
-
+    click.echo("Downloading source...")
     proc = subprocess.run(["pypi-download", package], capture_output=True)
     if proc.returncode != 0:
       click.echo("Failed to download source")
       return
 
+    click.echo("Extracting source...")
     tarballFilename = str(proc.stdout).split(" ")[1].split("\\")[0]
-    subprocess.run(["tar", "xvzf", tarballFilename])
+    subprocess.run(["tar", "xzf", tarballFilename])
 
+    click.echo("Checking for build dependencies...")
     if os.environ.get('BUILD_DEPENDENCIES'):
+      click.echo("Build dependencies found - installing...")
       subprocess.run(["apt-get", "install", "-y"] + BUILD_DEPENDENCIES.split(" "))
 
-    currentDir = os.getcwd()
+    click.echo("Moving source to top-level of repo...")
+    currentDir = pathlib.Path(os.getcwd())
     subDir = tarballFilename.replace(".tar.gz", "")
     for f in currentDir.rglob(f"{subDir}/*"):
         shutil.move(os.path.join(currentDir, subDir, f), currentDir)
 
-    subprocess.run(["ls", currentDir], capture_output=True)
+    click.echo("Moving source to top-level of repo...")
+    subprocess.run(["ls", currentDir])
+
     patchFilePath = pathlib.Path(os.path.join(currentDir, "pypi-build", "patches", f"{package}.patch"))
     print(f"Looking for patch: {patchFilePath}")
 
@@ -51,11 +54,21 @@ def main(optionsStr, package):
 
     if patchFilePath.exists():
       print(f"Patch found: {patchFilePath}")
-      args.append(f"--stdeb-patch-file={patchFilePath}")
+      args.append("-p")
+      args.append(str(patchFilePath))
 
-    for field in shlex.split(optionsStr):
+    for field in shlex.split(options_str):
       args.append(field)
       
-    args.append("--dist-dir=./artifacts")
+    args.append("--dist-dir")
+    args.append("./artifacts")
+
     args.append("bdist_deb")
-    subprocess.run(args, capture_output=True)
+
+    click.echo(args)
+
+    subprocess.run(args)
+
+
+if __name__ == "__main__":
+    main(prog_name="pypi-build")  # pragma: no cover
