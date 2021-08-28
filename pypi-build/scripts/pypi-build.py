65525a6cafecae53e290a976df65f27e9a95b699
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-08-28 19:13:49 +0100

    Update

--- pypi-build/scripts/pypi-build.py
+++ pypi-build/scripts/pypi-build.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 import click
 import os
-import subprocess
-import shlex
 import pathlib
+import shlex
 import shutil
-import os
+import subprocess
 
 
 @click.command()
@@ -33,19 +32,27 @@ def main(options_str, package):
     tarballFilename = str(proc.stdout).split(" ")[1].split("\\")[0]
     subprocess.run(["tar", "xzf", tarballFilename])
 
-    click.echo("Checking for build dependencies...")
-    if os.environ.get('BUILD_DEPENDENCIES'):
-      click.echo("Build dependencies found - installing...")
-      subprocess.run(["apt-get", "install", "-y"] + BUILD_DEPENDENCIES.split(" "))
-
     click.echo("Moving source to top-level of repo...")
     currentDir = pathlib.Path(os.getcwd())
     subDir = tarballFilename.replace(".tar.gz", "")
     for f in currentDir.rglob(f"{subDir}/*"):
         shutil.move(os.path.join(currentDir, subDir, f), currentDir)
 
-    click.echo("Moving source to top-level of repo...")
-    subprocess.run(["ls", currentDir])
+    click.echo("Removing tarball...")
+    os.remove(tarballFilename)
+
+    click.echo("Listing top-level of repo...")
+    subprocess.run(["ls", "-l", currentDir])
+
+    # Fix python-sonic broken source
+    # https://github.com/gkvoelkl/python-sonic/issues/38
+    with open('setup.py', 'r') as file :
+      filedata = file.read()
+
+    filedata = filedata.replace('PREADME.txt', 'README.rst')
+
+    with open('setup.py', 'w') as file:
+      file.write(filedata)
 
     patchFilePath = pathlib.Path(os.path.join(currentDir, "pypi-build", "patches", f"{package}.patch"))
     print(f"Looking for patch: {patchFilePath}")
@@ -54,18 +61,27 @@ def main(options_str, package):
 
     if patchFilePath.exists():
       print(f"Patch found: {patchFilePath}")
-      args.append("-p")
-      args.append(str(patchFilePath))
-
-    for field in shlex.split(options_str):
-      args.append(field)
+      for i in ["-p", str(patchFilePath), "-l", str(1)]:
+        args.append(i)
       
+    if options_str:
+      for field in shlex.split(options_str):
+        args.append(field)
+
     args.append("--dist-dir")
     args.append("./artifacts")
 
+    # Required for patching python-sonic's broken source
+    args.append("--ignore-source-changes")
+
     args.append("bdist_deb")
 
-    click.echo(args)
+    click.echo("Checking for build dependencies...")
+    if os.environ.get('BUILD_DEPENDENCIES'):
+      click.echo("Build dependencies found - installing...")
+      subprocess.run(["apt-get", "install", "-y"] + BUILD_DEPENDENCIES.split(" "))
+
+    click.echo(" ".join(args))
 
     subprocess.run(args)
 
